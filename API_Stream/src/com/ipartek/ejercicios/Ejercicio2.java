9392bd14f19ee44bdf6f5a62f043031137daf5e8
adridevelop <adridevelop11@gmail.com>
2025-11-18 09:35:47 +0100

    Finalizamos y comenzamos con el proyecto de Spring

--- API_Stream/src/com/ipartek/ejercicios/Ejercicio2.java
+++ API_Stream/src/com/ipartek/ejercicios/Ejercicio2.java
@@ -1,4 +1,27 @@
 package com.ipartek.ejercicios;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        // Utilizando programación funcional hacer una función que calcule el 21% del IVA
+        Function<Double, Double> calculaIva = precio -> precio * 0.21;
+
+        Double precioConIva = calculaIva.apply(120d);
+
+        System.out.println(precioConIva);
+
+        // Utilizando programación funcional hacer una función que calcule el precio total de un producto
+        // teniendo en cuenta que el tipo de IVA puede ser variable
+
+        BiFunction<Double, Integer, Double> calculaPrecioTotalProductoIvaVariable = (precio, iva) ->
+                precio + (precio * iva) / 100;
+
+        Double precioProductoTotalMasIva = calculaPrecioTotalProductoIvaVariable.apply(120d, 21);
+
+        System.out.println(precioProductoTotalMasIva);
+    }
 }
--- API_Stream/src/com/ipartek/ejercicios/Ejercicio4.java
+++
@@ -1,4 +0,0 @@
-package com.ipartek.ejercicios;
-
-public class Ejercicio4 {
-}
